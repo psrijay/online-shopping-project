510b0d12b99f9e803d52793245db68c0e908da85
psrijay <34514284+psrijay@users.noreply.github.com>
2017-12-13 21:01:02 +0530

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package com.onlineshopping.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
 
-/**
- *
- * @author iamsu
- */
+
 public class Category {
 
     private int id;
