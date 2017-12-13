ad04b9f772f72ad9f0396aae19101b967863225b
psrijay <34514284+psrijay@users.noreply.github.com>
2017-12-13 20:59:44 +0530

    Update CartDAOImpl.java

--- CartDAOImpl.java
+++ CartDAOImpl.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package com.onlineshopping.service;
 import com.onlineshopping.model.User;
@@ -16,10 +12,7 @@ import java.sql.ResultSet;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- *
- * @author iamsu
- */
+
 public class CartDAOImpl implements CartDAO{
 
     DataSource ds=new DataSource();
