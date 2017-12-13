1b23a2b899ef5c5a7f1c9ab063c6db4a64a5cf69
psrijay <34514284+psrijay@users.noreply.github.com>
2017-12-13 21:24:44 +0530

    Update DataConnector.java

--- DataConnector.java
+++ DataConnector.java
@@ -15,10 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
 
-/**
- *
- * @author iamsu
- */
+
 public class DataConnector implements DataSource{
 
     @Override
