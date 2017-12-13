49eac0aa6ac129fbc407666bd016fba55b981a5e
psrijay <34514284+psrijay@users.noreply.github.com>
2017-12-13 21:07:36 +0530

    Update ValidateMailControler.java

--- ValidateMailControler.java
+++ ValidateMailControler.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package com.onlineshopping.controller;
 
@@ -17,10 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author iamsu
- */
 @WebServlet(name = "ValidateMailControler", urlPatterns = {"/ValidateMail"})
 public class ValidateMailControler extends HttpServlet {
 
