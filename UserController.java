f0bb1ea7fbe9de6857b2d863300e44f6c2ef7300
psrijay <34514284+psrijay@users.noreply.github.com>
2017-12-13 21:09:33 +0530

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package com.onlineshopping.controller;
 
@@ -30,10 +26,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author iamsu
- */
+
 @WebServlet(name = "UserController", urlPatterns = {"/User"})
 public class UserController extends HttpServlet {
 
