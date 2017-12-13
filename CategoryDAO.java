6917313144ad5a04696d54af47e843b17c303601
psrijay <34514284+psrijay@users.noreply.github.com>
2017-12-13 21:02:43 +0530

    Update CategoryDAO.java

--- CategoryDAO.java
+++ CategoryDAO.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package com.onlineshopping.DAO;
 
 import com.onlineshopping.model.Category;
 import java.util.List;
 
-/**
- *
- * @author iamsu
- */
+
 public interface CategoryDAO {
     public void create(Category c);
     public Category read(Category c);
