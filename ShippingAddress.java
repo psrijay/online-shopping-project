7bb5efbeb7e7b028df523221ecc8c403db92fa68
psrijay <34514284+psrijay@users.noreply.github.com>
2017-12-13 21:11:53 +0530

    Update ShippingAddress.java

--- ShippingAddress.java
+++ ShippingAddress.java
@@ -1,15 +1,1 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-package com.onlineshopping.model;
-
-/**
- *
- * @author iamsu
- */
-public class ShippingAddress {
-    
-}
